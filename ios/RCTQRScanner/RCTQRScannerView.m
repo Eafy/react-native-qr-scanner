e98ea8e4243905523ae0ae0ebfb76f8dfd6aa365
lzj <lizhijian_21@163.com>
2019-04-08 14:49:26 +0800

    扫描成功0.5秒之后再次启动扫描

--- ios/RCTQRScanner/RCTQRScannerView.m
+++ ios/RCTQRScanner/RCTQRScannerView.m
@@ -75,8 +75,9 @@
     [self sendEventWithParams:event];
 
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        if (self.scanNative) {
-            [self.scanNative setNeedRereadQR:YES];     //再次启动扫描
+        if (self.scanNative && self.isStartScan) {
+//            [self.scanNative setNeedRereadQR:YES];     //再次启动扫描
+            [self.scanNative startScan];
         }
     });
 }
